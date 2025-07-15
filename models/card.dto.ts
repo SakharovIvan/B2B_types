4bda0c9fc4b9f7493d1fb2ca6f1c51d8d61da777
SakharovIvan <i731901@gmail.com>
2025-07-15 21:00:25 +0300

    models added

---
+++ models/card.dto.ts
@@ -0,0 +1,21 @@
+import { CardStructure, product } from '../common/card.types';
+
+export class Card_upd_dto implements CardStructure {
+  email: string;
+  card: product[]=[];
+  constructor(data) {
+    this.email = data.email;
+    this.card_init(data.card);
+  }
+  private card_init(data) {
+    data.forEach((el) => {
+      this.card.push({
+        spmatNo: el.spmatNo,
+        qty: el.qty,
+        Warranty: el.Warranty || false,
+        WarrantyRepNo: el.WarrantyRepNo || null,
+        UserComment: el.UserComment || null,
+      });
+    });
+  }
+}
