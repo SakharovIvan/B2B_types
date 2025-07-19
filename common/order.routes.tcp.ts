8705ba33071c3c796dc374040943aae82a88abb1
SakharovIvan <i731901@gmail.com>
2025-07-19 21:47:44 +0300

    c1 tcp connection added

--- common/order.routes.tcp.ts
+++ common/order.routes.tcp.ts
@@ -1,24 +1,37 @@
-export enum Order_cmd{
-    create_order='create_order_from_card',
-    update='update_order',
-    get_orders_list='get_orders_list',
-    get_order_content='get_order_info',
+import { C1_Order, C1_order_req } from "./order.type";
+
+export enum Order_cmd {
+  create_order = 'create_order_from_card',
+  update = 'update_order',
+  get_orders_list = 'get_orders_list',
+  get_order_content = 'get_order_info',
+  c1_get_order = 'c1_get_order',
+  c1_upd_order = 'c1_upd_order',
+  c1_get_order_test = 'c1_get_order_test',
+  c1_upd_order_test = 'c1_upd_order_test',
+  c1_clear_order_test = 'c1_clear_order_test',
+}
+
+export interface Order_cmd_int{
+    cmd:Order_cmd,
+    request?:Partial<C1_Order>
+    data?:C1_order_req
 }
 
 export interface create_order {
-    data:{
-        card:Object[],
-        email:string
-    }
+  data: {
+    card: Object[];
+    email: string;
+  };
 }
 
-export interface get_order_list{
-    data:{
-        filters: Object;
-            options?: { offset: number; limit: number };
-    }
+export interface get_order_list {
+  data: {
+    filters: Object;
+    options?: { offset: number; limit: number };
+  };
 }
 
-export interface get_order_content{
-    data: { email: string; OrderNo: number }
+export interface get_order_content {
+  data: { email: string; OrderNo: number };
 }
--- common/order.type.ts
+++ common/order.type.ts
@@ -1,27 +1,35 @@
 import { product } from './card.types';
-export interface C1_Order {
+import { ASCInfo } from './client.types';
+
+interface C1_Order_Main {
   id?: string;
+  C1_code: string;
+  status: number;
+  GUID_1C: string;
+}
+export interface C1_Order extends C1_Order_Main {
   OrderNo: number;
   B2b_id: string;
   UPD_param: boolean;
-  GUID_1C: string;
-  C1_code: string;
-  status: number;
   User_id: string;
   sum_wo_skid: number;
   sum_skid: number;
   sum_NDS: number;
   sum: number;
+  SumPriceNDS: number;
+  SumPrice_NDS: number;
+  SumPrice_wo_NDS: number;
+
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 export interface C1_OrderMatNo {
   id?: string;
-  spmatNo: number;
+  spmatNo: string;
   qty: number;
   price: number;
-  orderid: number;
+  orderid: string;
   price_wo_skid: number;
   price_skid: number;
   price_rate_NDS: number;
@@ -30,13 +38,26 @@ export interface C1_OrderMatNo {
   createdAt?: Date;
   updatedAt?: Date;
 }
+export interface C1_order_req extends C1_Order_Main {
+  User_info?: ASCInfo;
+  content: C1_OrderMatNo[];
+  SumPriceNDS: number;
+  SumPrice_NDS: number;
+  SumPrice_wo_NDS: number;
+}
 
-export interface OrderMatNo extends product{
+export interface OrderMatNo extends product {
   id?: string;
   SystemCheckErrorNo: number;
   createdAt?: Date;
   updateTimestamp?: Date;
   UserOrderId: string;
+}
+
+export interface C1_Order_Info extends C1_Order {
+  User_info?: ASCInfo;
+
+  products: C1_OrderMatNo[];
 }
 
 export interface Order {
@@ -47,19 +68,19 @@ export interface Order {
   updatedAt?: Date;
 }
 
-export interface OrderStatus{
-  id?:string;
-  SystemCheck:boolean;
-  WarrantyCheck:boolean;
-  ManagerCheck:boolean;
-  UserPayCheck:boolean;
-  UserDeliverCheck:boolean;
-  createdAt?:Date;
-  updateTimestamp?:Date;
-  UserOrderId:string;
+export interface OrderStatus {
+  id?: string;
+  SystemCheck: boolean;
+  WarrantyCheck: boolean;
+  ManagerCheck: boolean;
+  UserPayCheck: boolean;
+  UserDeliverCheck: boolean;
+  createdAt?: Date;
+  updateTimestamp?: Date;
+  UserOrderId: string;
 }
 
-export enum ORDER_MAIL_VIEWS{
+export enum ORDER_MAIL_VIEWS {
   'CREATED',
-  'UPDATED'
+  'UPDATED',
 }
