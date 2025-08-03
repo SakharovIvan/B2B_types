dd89190989839782a3d3e9b086be68a14d1ec395
SakharovIvan <i731901@gmail.com>
2025-08-04 00:32:39 +0300

    new card tcp for favorite list

--- common/card.routes.tcp.ts
+++ common/card.routes.tcp.ts
@@ -5,6 +5,6 @@ export enum Card_cmd{
     delete_user_card='delete_user_card',//+
     check_user_card='check_user_card',
     get_admin_card='get_card_admin',//+
-    create_order='create_order'//+
-
+    create_order='create_order',//+
+    warehouse_upd_favorite_List='warehouse_upd_favorite_List'
 }
