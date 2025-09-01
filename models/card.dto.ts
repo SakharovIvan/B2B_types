dc60a107b530d5d55d9f4286160e1c2b1571fb48
SakharovIvan <i731901@gmail.com>
2025-09-01 13:40:38 +0300

    product dto for card

--- models/card.dto.ts
+++ models/card.dto.ts
@@ -9,15 +9,25 @@ export class Card_upd_dto implements CardStructure {
   }
   private card_init(data) {
     data.forEach((el) => {
-      let product: Partial<product> = {};
-
-      if (el.qty) {
-        product.qty = Number(el.qty);
-      }
-      this.card.push({
-        ...el,
-        ...product,
-      });
+      const product = new Product_DTO(el)
+      this.card.push(product);
     });
+  }
+}
+
+export class Product_DTO implements product {
+  spmatNo;
+  qty;
+  Warranty;
+  WarrantyRepNo;
+  UserComment;
+  constructor(data) {
+    this.spmatNo=data.spmatNo
+    this.qty=data.qty
+    this.Warranty=data.Warranty||false
+    if(data.WarrantyRepNo){
+    this.WarrantyRepNo=data.WarrantyRepNo}
+    if(data.UserComment){
+    this.UserComment=data.UserComment}
   }
 }
